c5d0a9c486839085d97f260a9719874d164349e8
Muhammad Imran Bin Ridzuan <imran.ridzuan@shopee.com>
2022-07-12 18:10:55 +0800

    Complete js exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    for (let i = 0; i < toRemove.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === toRemove[i]) {
+                arr.splice(j,1)
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
