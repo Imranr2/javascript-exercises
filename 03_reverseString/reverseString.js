38038c99f9ee44a23cdeeda0c925482575280c16
Muhammad Imran Bin Ridzuan <imran.ridzuan@shopee.com>
2022-07-12 18:04:44 +0800

    Complete js exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArr = string.split("")
+    let reverseStringArr = stringArr.reverse()
+    return reverseStringArr.join('')
 };
 
 // Do not edit below this line
