059ac8e144465c284d2de35074a8ecef0c3dfdba
Muhammad Imran Bin Ridzuan <imran.ridzuan@shopee.com>
2022-07-12 18:01:24 +0800

    Complete js exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let repeatStr = []
+    for (let i = 0; i < num; i++) {
+        repeatStr.push(string)
+    }
+    return repeatStr.join('')
 };
 
 // Do not edit below this line
